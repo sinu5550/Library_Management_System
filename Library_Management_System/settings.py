67002dfeb6da7ef29c7a829f9a70370e5552eee3
Intisar Ahmed Siyan <intisar.siyan5550@gmail.com>
2024-01-18 10:48:26 +0600

    Littlebit change

--- Library_Management_System/settings.py
+++ Library_Management_System/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-om1l%$yka--o@-!*oxw1pu2x2z4@fj#-$lt#1c3*@-@))@s8jt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
